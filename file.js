06635257a6bd22f449cb6940489f4d599f423b20
Rosario Nici <rosarionici95@gmail.com>
2022-09-13 18:14:19 +0200

    secon jacks

--- file.js
+++ file.js
@@ -90,3 +90,6 @@ console.log(newSecondaryArray)
  * Dividi in due array separati le zucchine che misurano meno o più di 15cm.
  * Infine stampa separatamente quanto pesano i due gruppi di zucchine.
  */
+
+
+
